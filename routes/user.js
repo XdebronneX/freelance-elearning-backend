8371d0784ecb5d0c1bd8d2125e91ed00f56a0def
XdebronneX <martelevans42@gmail.com>
2024-09-02 12:29:28 +0800

    combine update password and update information

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth")
-const { registerUser, loginUser, getProfile, updatePassword, updateProfile, forgotPassword, resetPassword, logoutUser, verifyOTP, resendOTP} = require("../controllers/userController");
+const { registerUser, loginUser, getProfile, updateProfile, forgotPassword, resetPassword, logoutUser, verifyOTP, resendOTP} = require("../controllers/userController");
 const upload = require("../utils/multerImg");
 
 router.route('/register').post(registerUser);
@@ -9,7 +9,7 @@ router.route('/send/verification').post(resendOTP);
 router.route('/verification').post(verifyOTP);
 router.route('/login').post(loginUser);
 router.get('/me', isAuthenticatedUser, getProfile);
-router.put('/me/changePassword', isAuthenticatedUser, updatePassword);
+// router.put('/me/changePassword', isAuthenticatedUser, updatePassword);
 router.put('/me/update', isAuthenticatedUser, upload.single('avatar'), updateProfile);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset').put(resetPassword);
