b2a995889890c81d5c5347a58cd8d45b3459ff0b
XdebronneX <martelevans42@gmail.com>
2024-09-03 19:10:25 +0800

    rollback

--- utils/multerImg.js
+++ utils/multerImg.js
@@ -2,23 +2,15 @@ const multer = require("multer");
 const path = require("path");
 const ErrorHandler = require("./errorHandler");
 
-// Change storage to memory storage
-const storage = multer.memoryStorage(); 
-
-const fileFilter = (req, file, cb) => {
-  const ext = path.extname(file.originalname).toLowerCase();
-  if (![".jpg", ".jpeg", ".png", ".mp4"].includes(ext)) {
-    cb(new ErrorHandler("Unsupported file type!"), false);
-    return;
-  }
-  cb(null, true);
-};
-
-const upload = multer({ 
-  storage, 
-  limits: { fileSize: 100 * 1024 * 1024 }, // 100 MB limit
-  fileFilter 
+module.exports = multer({
+    limits: { fieldSize: 30 * 1024 * 1024 },
+    storage: multer.diskStorage({}),
+    fileFilter: (req, file, cb) => {
+        let ext = path.extname(file.originalname).toLowerCase();
+        if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".mp4") {
+            cb(new ErrorHandler("Unsupported file type!"), false);
+            return;
+        }
+        cb(null, true);
+    },
 });
-
-module.exports = upload;
-
