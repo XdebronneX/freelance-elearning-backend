a958ccf4ec871b6b3d1e17f3bc9924ac1c1a4548
XdebronneX <martelevans42@gmail.com>
2024-09-03 18:35:37 +0800

    save storage to memory

--- utils/multerImg.js
+++ utils/multerImg.js
@@ -2,16 +2,23 @@ const multer = require("multer");
 const path = require("path");
 const ErrorHandler = require("./errorHandler");
 
-module.exports = multer({
-    limits: { fieldSize: 20 * 1024 * 1024 },
-    storage: multer.diskStorage({}),
-    fileFilter: (req, file, cb) => {
-        let ext = path.extname(file.originalname).toLowerCase();
+// Change storage to memory storage
+const storage = multer.memoryStorage(); 
 
-        if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-            cb(new ErrorHandler("Unsupported file type!"), false);
-            return;
-        }
-        cb(null, true);
-    },
+const fileFilter = (req, file, cb) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (![".jpg", ".jpeg", ".png", ".mp4"].includes(ext)) {
+    cb(new ErrorHandler("Unsupported file type!"), false);
+    return;
+  }
+  cb(null, true);
+};
+
+const upload = multer({ 
+  storage, 
+  limits: { fileSize: 100 * 1024 * 1024 }, // 100 MB limit
+  fileFilter 
 });
+
+module.exports = upload;
+
