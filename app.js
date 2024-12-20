d6d720eef2571a9a2d8874dbd0bfd3fa25b1fa7b
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-12-20 12:25:21 +0800

    remove other unused code

--- app.js
+++ app.js
@@ -5,38 +5,27 @@ const cors = require("cors");
 
 const errorMiddleware = require("./middlewares/errors");
 const users = require("./routes/user");
-const courses = require("./routes/course");
-const carousels = require("./routes/carousel");
-const analytics = require("./routes/analytic");
-const lessons = require("./routes/lesson");
-const progressRoute = require("./routes/progress")
-const customizationRoute = require("./routes/customization")
+
 // List of allowed origins
-const allowedOrigins = [ "http://localhost:3000",
-"https://e-learning-freelance-frontend.vercel.app"];
+const allowedOrigins = ["http://localhost:3000"];
 // Middleware
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.urlencoded({  limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
-  },
-  credentials: true,
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }));
 
 // Routes
 app.use("/api/v1", users);
-app.use("/api/v1", courses);
-app.use("/api/v1", carousels);
-app.use("/api/v1", lessons);
-app.use("/api/v1", progressRoute);
-app.use("/api/v1", analytics);
-app.use("/api/v1", customizationRoute);
+
 // Error Middleware
 app.use(errorMiddleware);
 
